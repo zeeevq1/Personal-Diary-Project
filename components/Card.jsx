e51a78fa3c142cabcda7cdd65db2f4c610e4c0ae
Mateusz Olszewski <zeeevq1@gmail.com>
2025-08-22 16:40:51 +0200

    Form.jsx and Card.jsx

---
+++ components/Card.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import Form from "./Form";
+
+function Card({ title, date, image, description }) {
+  return (
+    <div>
+      <div className="card bg-base-100 w-96 shadow-sm">
+        <figure>
+          <img className="w-full object-cover" src={image} alt={title} />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{title}</h2>
+          <p>{description}</p>
+          <p>
+            <strong>Date:</strong> {date}
+          </p>
+          <div className="card-actions justify-end">
+            <button className="bg-green-400">Checked</button>
+            <button className="bg-red-400">Delete</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
--- components/Form.jsx
+++ components/Form.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Card from "./Card.jsx";
 
 function Form() {
   const [form, setForm] = useState({
@@ -7,80 +8,92 @@ function Form() {
     image: "",
     description: "",
   });
+
+  const [submitted, setSubmittedData] = useState(null);
+
   const handleChange = (e) => {
-    setForm((formVal) => ({ ...formVal, [e.target.name]: e.target.value }));
+    const { name, value } = e.target;
+    setForm((prev) => ({ ...prev, [name]: value }));
   };
   const handleSubmit = (e) => {
     e.preventDefault(e.target.elements);
 
-    console.log(e.target.elements);
-    const { title, date, image, description, button } = e.target.elements;
     try {
-      button.disabled = true;
-      if (!title.value.trim()) throw new Error("Name is required");
-      if (!date.value.trim()) throw new Error("Date is required");
-      if (!image.value.trim()) throw new Error("Image is required");
-      if (!description.value.trim()) throw new Error("Description is required");
+      if (!form.title.trim()) throw new Error("Name is required");
+      if (!form.date.trim()) throw new Error("Date is required");
+      if (!form.image.trim()) throw new Error("Image is required");
+      if (!form.description.trim()) throw new Error("Description is required");
+
+      setSubmittedData(form);
+
       setForm({ title: "", date: "", image: "", description: "" });
     } catch (error) {
       alert(error.message);
-    } finally {
-      button.disabled = false;
     }
   };
+
   return (
     <div className="w-[400px]">
-      <form className="flex flex-col w-full gap-4" onSubmit={handleSubmit}>
-        <label htmlFor="">
-          Title
-          <input
-            type="text"
-            name="title"
-            id="title"
-            value={form.title}
-            placeholder="Title"
-            className="input input-bordered w-full"
-            onChange={handleChange}
-          />
-        </label>
-        <label htmlFor="">
-          Date
-          <input
-            type="date"
-            name="date"
-            value={form.date}
-            inputMode="numeric"
-            className="input input-bordered w-full"
-            onChange={handleChange}
-          />
-        </label>
-        <label htmlFor="">
-          Image
-          <input
-            name="image"
-            id="image"
-            type="text"
-            value={form.image}
-            placeholder="Image URL"
-            className="input input-bordered w-full"
-            onChange={handleChange}
-          />
-        </label>
-        <label htmlFor="">
-          Text
-          <textarea
-            name="description"
-            id="description"
-            value={form.description}
-            placeholder="Content"
-            className="textarea textarea-bordered w-full"
-            onChange={handleChange}
-          />
-        </label>
-        <button name="button" type="submit" className="btn btn-primary mt-2">
-          Submit
-        </button>
-      </form>
+      {!submitted ? (
+        <form className="flex flex-col w-full gap-4" onSubmit={handleSubmit}>
+          <label>
+            Title
+            <input
+              type="text"
+              name="title"
+              value={form.title}
+              placeholder="Title"
+              className="input input-bordered w-full"
+              onChange={handleChange}
+            />
+          </label>
+
+          <label>
+            Date
+            <input
+              type="date"
+              name="date"
+              value={form.date}
+              className="input input-bordered w-full"
+              onChange={handleChange}
+            />
+          </label>
+
+          <label>
+            Image
+            <input
+              type="text"
+              name="image"
+              value={form.image}
+              placeholder="Image URL"
+              className="input input-bordered w-full"
+              onChange={handleChange}
+            />
+          </label>
+
+          <label>
+            Text
+            <textarea
+              name="description"
+              value={form.description}
+              placeholder="Content"
+              className="textarea textarea-bordered w-full"
+              onChange={handleChange}
+            />
+          </label>
+
+          <button type="submit" className="btn btn-primary mt-2">
+            Submit
+          </button>
+        </form>
+      ) : (
+        <Card
+          title={submitted.title}
+          date={submitted.date}
+          image={submitted.image}
+          description={submitted.description}
+        />
+      )}
     </div>
   );
 }
