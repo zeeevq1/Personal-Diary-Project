31ae56f1d0f6a17139e9ecb4300abc25fb074993
Vibhutibahen Sangani <sanganivibhuti@gmail.com>
2025-08-25 16:01:52 +0200

    added logo in header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,14 @@
 const Header = () => {
   return (
-    <header className="w-full bg-black p-4">
-      <nav>
-        <button className="btn btn-success btn-sm">Create Diary</button>
+    <header className="w-full bg-black py-1">
+      <nav className="flex justify-between items-center">
+        <img
+          src="./assets/logo.png"
+          className="w-[80px] h-[80px] ml-[1rem]"
+        ></img>
+        <button className="btn btn-success btn-sm mr-[1.9rem]">
+          Create Diary
+        </button>
       </nav>
     </header>
   );
